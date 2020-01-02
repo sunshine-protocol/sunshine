5e4452e1d82e2f39a1500badaa6df4f71fc7e9e7
4meta5 <asinghchrony@protonmail.com>
2020-01-01 21:22:13 -0500

    more changes

--- modules/committee/src/lib.rs
+++ modules/committee/src/lib.rs
@@ -1,2 +1,12 @@
 // committee
 // must be instanceable like a `council`
+
+/// Trait for type that can handle incremental changes to a set of account IDs.
+pub trait OnMembersChanged<AccountId> {
+	/// A number of members `new` just joined the set and replaced some `old` ones.
+	fn on_members_changed(new: &[AccountId], old: &[AccountId]);
+}
+
+impl<T> OnMembersChanged<T> for () {
+	fn on_members_changed(_new: &[T], _old: &[T]) {}
+}
