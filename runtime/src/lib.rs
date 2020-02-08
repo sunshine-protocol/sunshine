dbb88f87d1e3b5dd0b8e6f653a3a06bcd2a341fe
4meta5 <asinghchrony@protonmail.com>
2020-02-08 01:00:16 -0500

    add clippy prelint for substrate macro unsafe pointer usage

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -3,6 +3,7 @@
 // `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
 #![recursion_limit = "256"]
 #![allow(clippy::identity_op)]
+#![allow(clippy::not_unsafe_ptr_arg_deref)]
 // Make the WASM binary available.
 #[cfg(feature = "std")]
 include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
