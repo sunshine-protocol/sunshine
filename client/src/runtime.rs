e4d2ff889b5d1f15be91b08384eb962bbc7c3323
David Craven <david@craven.ch>
2020-04-15 14:22:40 +0200

    Get example working.

--- client/src/runtime.rs
+++ client/src/runtime.rs
@@ -6,8 +6,6 @@ use sp_runtime::{
     transaction_validity::TransactionValidityError,
     MultiSignature, OpaqueExtrinsic,
 };
-use std::collections::HashMap;
-use std::convert::TryFrom;
 use std::marker::PhantomData;
 use substrate_subxt::{
     balances::{AccountData, Balances},
@@ -18,7 +16,6 @@ use substrate_subxt::{
     CheckVersion,
     CheckWeight,
     SignedExtra,
-    //EventsDecoder, EventsError, Metadata,
 };
 
 /// Concrete type definitions compatible w/ sunshine's runtime aka `suntime`
@@ -94,37 +91,3 @@ impl<T: System + Balances + Send + Sync> SignedExtension for Extra<T> {
         self.extra().additional_signed()
     }
 }
-
-/*impl TryFrom<Metadata> for EventsDecoder<Runtime> {
-    type Error = EventsError;
-
-    fn try_from(metadata: Metadata) -> Result<Self, Self::Error> {
-        let mut decoder = Self {
-            metadata,
-            type_sizes: HashMap::new(),
-            marker: PhantomData,
-        };
-        // register default event arg type sizes for dynamic decoding of events
-        decoder.register_type_size::<bool>("bool")?;
-        decoder.register_type_size::<u32>("ReferendumIndex")?;
-        decoder.register_type_size::<[u8; 16]>("Kind")?;
-        decoder.register_type_size::<[u8; 32]>("AuthorityId")?;
-        decoder.register_type_size::<u8>("u8")?;
-        decoder.register_type_size::<u32>("u32")?;
-        decoder.register_type_size::<u32>("AccountIndex")?;
-        decoder.register_type_size::<u32>("SessionIndex")?;
-        decoder.register_type_size::<u32>("PropIndex")?;
-        decoder.register_type_size::<u32>("ProposalIndex")?;
-        decoder.register_type_size::<u32>("AuthorityIndex")?;
-        decoder.register_type_size::<u64>("AuthorityWeight")?;
-        decoder.register_type_size::<u32>("MemberCount")?;
-        decoder.register_type_size::<Runtime::AccountId>("AccountId")?;
-        decoder.register_type_size::<Runtime::BlockNumber>("BlockNumber")?;
-        decoder.register_type_size::<Runtime::Hash>("Hash")?;
-        decoder.register_type_size::<<Runtime as Balances>::Balance>("Balance")?;
-        // VoteThreshold enum index
-        decoder.register_type_size::<u8>("VoteThreshold")?;
-
-        Ok(decoder)
-    }
-}*/
