5ddbeb98bfc574b51f6a417bd04adbcb88baa5e7
David Craven <david@craven.ch>
2020-04-16 16:53:31 +0200

    Prepare node for light client.

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,14 +1,3 @@
-//! Node CLI
-#![allow(clippy::too_many_arguments)]
-#![allow(clippy::clone_double_ref)]
-#![warn(missing_docs)]
-
-mod chain_spec;
-#[macro_use]
-mod service;
-mod cli;
-mod command;
-
 fn main() -> sc_cli::Result<()> {
-    command::run()
+    sunshine_node::run_cli()
 }
