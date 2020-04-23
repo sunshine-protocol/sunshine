47d412e2ad413d7edfacaf10681107c25f398268
David Craven <david@craven.ch>
2020-04-23 13:08:12 +0200

    Use subxt-proc-macro.

--- client/examples/reserve_shares_and_watch.rs
+++ client/examples/reserve_shares_and_watch.rs
@@ -1,8 +1,6 @@
 use sp_core::crypto::Pair;
 use sp_keyring::AccountKeyring;
 use sunshine_client::shares_atomic::*;
-use sunshine_client::system::System;
-use sunshine_client::Runtime;
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -15,23 +13,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let share = 1;
     let account = signer.public().into();
     let reserved = client
-        .profile(&org, &share, &account)
+        .profile(&(&org, &share), &account)
         .await?
         .get_times_reserved();
 
     let extrinsic_success = xt
         .watch()
         .with_shares_atomic()
-        .submit(reserve_shares::<Runtime>(org, share, account.clone()))
+        .reserve_shares(org, share, account.clone())
         .await?;
-    let res = extrinsic_success.shares_reserved().unwrap()?;
+    let event = extrinsic_success.shares_reserved().unwrap()?;
 
-    assert_eq!((org, share, account, reserved + 1), res);
+    assert_eq!(reserved + 1, event.reserved);
 
-    let (org, share, account, reserved) = res;
     println!(
         "Account {:?} reserved {:?} shares with share id {:?} for organization {:?}",
-        account, reserved, share, org,
+        event.account, event.reserved, event.share, event.org,
     );
 
     Ok(())
--- client/src/frame/shares_atomic.rs
+++ client/src/frame/shares_atomic.rs
@@ -1,14 +1,13 @@
-use codec::{Codec, Error as CodecError};
+use codec::{Codec, Decode, Encode};
 use frame_support::Parameter;
-use futures::future;
 use sp_runtime::traits::{AtLeast32Bit, MaybeSerializeDeserialize, Member, Zero};
 use std::fmt::Debug;
-use std::future::Future;
-use std::pin::Pin;
-use substrate_subxt::{system::System, Call, Client, EventsSubscriber, ExtrinsicSuccess};
+use substrate_subxt::system::System;
+use substrate_subxt_proc_macro::*;
 use util::share::AtomicShareProfile;
 
 /// The subset of the `shares_atomic::Trait` that a client must implement.
+#[module]
 pub trait SharesAtomic: System {
     type OrgId: Parameter
         + Member
@@ -40,197 +39,143 @@ pub trait SharesAtomic: System {
         + Zero;
 }
 
-pub trait SharesAtomicEventsDecoder {
-    fn with_shares_atomic(self) -> Self;
-}
+#[storage]
+pub trait SharesAtomicStore<T: SharesAtomic> {
+    fn share_id_counter(org: &T::OrgId) -> T::ShareId;
 
-impl<T: SharesAtomic, P, S, E> SharesAtomicEventsDecoder for EventsSubscriber<T, P, S, E> {
-    fn with_shares_atomic(self) -> Self {
-        self.events_decoder(|decoder| {
-            decoder.register_type_size::<T::OrgId>("OrgId")?;
-            decoder.register_type_size::<T::ShareId>("ShareId")?;
-            decoder.register_type_size::<T::Share>("Share")
-        })
-    }
-}
+    fn total_issuance(org: &T::OrgId, share: &T::ShareId) -> T::Share;
 
-const MODULE: &str = "SharesAtomic";
+    fn share_holders(org: &T::OrgId, share: &T::ShareId) -> Vec<<T as System>::AccountId>;
+
+    fn profile(
+        prefix: &(&T::OrgId, &T::ShareId),
+        account_id: &<T as System>::AccountId,
+    ) -> AtomicShareProfile<T::Share>;
+}
 
 /// Register shares.
-pub fn register_shares<T: SharesAtomic>(
-    org: T::OrgId,
-    share: T::ShareId,
+#[derive(Call, Debug, Encode)]
+pub struct RegisterSharesCall<T: SharesAtomic> {
+    organization: T::OrgId,
+    share_id: T::ShareId,
     genesis: Vec<(T::AccountId, T::Share)>,
-) -> Call<(T::OrgId, T::ShareId, Vec<(T::AccountId, T::Share)>)> {
-    Call::new(MODULE, "register_shares", (org, share, genesis))
+}
+
+#[derive(Call, Debug, Encode)]
+pub struct LockSharesCall<T: SharesAtomic> {
+    organization: T::OrgId,
+    share_id: T::ShareId,
+    who: <T as System>::AccountId,
+}
+
+#[derive(Call, Debug, Encode)]
+pub struct UnlockSharesCall<T: SharesAtomic> {
+    organization: T::OrgId,
+    share_id: T::ShareId,
+    who: <T as System>::AccountId,
 }
 
 /// Request the share reservation.
-pub fn reserve_shares<T: SharesAtomic>(
-    org: T::OrgId,
-    share: T::ShareId,
-    account: <T as System>::AccountId,
-) -> Call<(T::OrgId, T::ShareId, <T as System>::AccountId)> {
-    Call::new(MODULE, "reserve_shares", (org, share, account))
+#[derive(Call, Debug, Encode)]
+pub struct ReserveSharesCall<T: SharesAtomic> {
+    organization: T::OrgId,
+    share_id: T::ShareId,
+    who: <T as System>::AccountId,
 }
 
-type EventResult<T> = Option<Result<T, CodecError>>;
-
-pub trait SharesAtomicEvents<T: SharesAtomic> {
-    fn shares_reserved(&self)
-        -> EventResult<(T::OrgId, T::ShareId, <T as System>::AccountId, u32)>;
-    fn shares_unreserved(
-        &self,
-    ) -> EventResult<(T::OrgId, T::ShareId, <T as System>::AccountId, u32)>;
-    fn shares_locked(&self) -> EventResult<(T::OrgId, T::ShareId, <T as System>::AccountId)>;
-    fn shares_unlocked(&self) -> EventResult<(T::OrgId, T::ShareId, <T as System>::AccountId)>;
-    fn new_share_type(&self) -> EventResult<(T::OrgId, T::ShareId)>;
-    fn issuance(&self) -> EventResult<(T::OrgId, T::ShareId, <T as System>::AccountId, T::Share)>;
-    fn burn(&self) -> EventResult<(T::OrgId, T::ShareId, <T as System>::AccountId, T::Share)>;
-    fn total_shares_issued(&self) -> EventResult<(T::OrgId, T::ShareId, T::Share)>;
+#[derive(Call, Debug, Encode)]
+pub struct UnreserveSharesCall<T: SharesAtomic> {
+    organization: T::OrgId,
+    share_id: T::ShareId,
+    who: <T as System>::AccountId,
 }
 
-impl<T: SharesAtomic> SharesAtomicEvents<T> for ExtrinsicSuccess<T> {
-    fn shares_reserved(
-        &self,
-    ) -> EventResult<(T::OrgId, T::ShareId, <T as System>::AccountId, u32)> {
-        self.find_event::<(T::OrgId, T::ShareId, <T as System>::AccountId, u32)>(
-            MODULE,
-            "SharesReserved",
-        )
-    }
-
-    fn shares_unreserved(
-        &self,
-    ) -> EventResult<(T::OrgId, T::ShareId, <T as System>::AccountId, u32)> {
-        self.find_event::<(T::OrgId, T::ShareId, <T as System>::AccountId, u32)>(
-            MODULE,
-            "SharesUnReserved",
-        )
-    }
-
-    fn shares_locked(&self) -> EventResult<(T::OrgId, T::ShareId, <T as System>::AccountId)> {
-        self.find_event::<(T::OrgId, T::ShareId, <T as System>::AccountId)>(MODULE, "SharesLocked")
-    }
-
-    fn shares_unlocked(&self) -> EventResult<(T::OrgId, T::ShareId, <T as System>::AccountId)> {
-        self.find_event::<(T::OrgId, T::ShareId, <T as System>::AccountId)>(
-            MODULE,
-            "SharesUnlocked",
-        )
-    }
-
-    fn new_share_type(&self) -> EventResult<(T::OrgId, T::ShareId)> {
-        self.find_event::<(T::OrgId, T::ShareId)>(MODULE, "NewShareType")
-    }
-
-    fn issuance(&self) -> EventResult<(T::OrgId, T::ShareId, <T as System>::AccountId, T::Share)> {
-        self.find_event::<(T::OrgId, T::ShareId, <T as System>::AccountId, T::Share)>(
-            MODULE, "Issuance",
-        )
-    }
-
-    fn burn(&self) -> EventResult<(T::OrgId, T::ShareId, <T as System>::AccountId, T::Share)> {
-        self.find_event::<(T::OrgId, T::ShareId, <T as System>::AccountId, T::Share)>(
-            MODULE, "Burn",
-        )
-    }
-
-    fn total_shares_issued(&self) -> EventResult<(T::OrgId, T::ShareId, T::Share)> {
-        self.find_event::<(T::OrgId, T::ShareId, T::Share)>(MODULE, "TotalSharesIssued")
-    }
+#[derive(Call, Debug, Encode)]
+pub struct IssueSharesCall<T: SharesAtomic> {
+    organization: T::OrgId,
+    share_id: T::ShareId,
+    who: <T as System>::AccountId,
+    shares: T::Share,
 }
 
-type StoreResult<'a, T> =
-    Pin<Box<dyn Future<Output = Result<T, substrate_subxt::Error>> + Send + 'a>>;
-
-pub trait SharesAtomicStore<T: SharesAtomic> {
-    fn total_issuance<'a>(
-        &'a self,
-        org: &T::OrgId,
-        share: &T::ShareId,
-    ) -> StoreResult<'a, T::Share>;
-    fn profile<'a>(
-        &'a self,
-        org: &T::OrgId,
-        share: &T::ShareId,
-        account_id: &<T as System>::AccountId,
-    ) -> StoreResult<'a, AtomicShareProfile<T::Share>>;
+#[derive(Call, Debug, Encode)]
+pub struct BurnSharesCall<T: SharesAtomic> {
+    organization: T::OrgId,
+    share_id: T::ShareId,
+    who: <T as System>::AccountId,
+    shares: T::Share,
 }
 
-impl<T, S, E> SharesAtomicStore<T> for Client<T, S, E>
-where
-    T: SharesAtomic + Send + Sync,
-    S: 'static,
-    E: Send + Sync + 'static,
-{
-    fn total_issuance<'a>(
-        &'a self,
-        org: &T::OrgId,
-        share: &T::ShareId,
-    ) -> StoreResult<'a, T::Share> {
-        let map_fn = || {
-            Ok(self
-                .metadata()
-                .module(MODULE)?
-                .storage("TotalIssuance")?
-                .map()?)
-        };
-        let map = match map_fn() {
-            Ok(map) => map,
-            Err(err) => return Box::pin(future::err(err)),
-        };
-        let future = self.fetch(map.key(&(org, share)), None);
-        Box::pin(async move {
-            let v = if let Some(v) = future.await? {
-                Some(v)
-            } else {
-                map.default().cloned()
-            };
-            Ok(v.unwrap_or_default())
-        })
-    }
+#[derive(Debug, Decode, Eq, Event, PartialEq)]
+pub struct SharesReservedEvent<T: SharesAtomic> {
+    pub org: T::OrgId,
+    pub share: T::ShareId,
+    pub account: <T as System>::AccountId,
+    pub reserved: u32,
+}
 
-    fn profile<'a>(
-        &'a self,
-        org: &T::OrgId,
-        share: &T::ShareId,
-        account: &<T as System>::AccountId,
-    ) -> StoreResult<'a, AtomicShareProfile<T::Share>> {
-        let map_fn = || {
-            Ok(self
-                .metadata()
-                .module(MODULE)?
-                .storage("Profile")?
-                .double_map()?)
-        };
-        let map = match map_fn() {
-            Ok(map) => map,
-            Err(err) => return Box::pin(future::err(err)),
-        };
-        let future = self.fetch(map.key(&(org, share), account), None);
-        Box::pin(async move {
-            let v = if let Some(v) = future.await? {
-                Some(v)
-            } else {
-                map.default().cloned()
-            };
-            Ok(v.unwrap_or_default())
-        })
-    }
+#[derive(Debug, Decode, Eq, Event, PartialEq)]
+pub struct SharesUnReservedEvent<T: SharesAtomic> {
+    pub org: T::OrgId,
+    pub share: T::ShareId,
+    pub account: <T as System>::AccountId,
+    pub reserved: u32,
+}
+
+#[derive(Debug, Decode, Eq, Event, PartialEq)]
+pub struct SharesLockedEvent<T: SharesAtomic> {
+    pub org: T::OrgId,
+    pub share: T::ShareId,
+    pub account: <T as System>::AccountId,
+}
+
+#[derive(Debug, Decode, Eq, Event, PartialEq)]
+pub struct SharesUnlockedEvent<T: SharesAtomic> {
+    pub org: T::OrgId,
+    pub share: T::ShareId,
+    pub account: <T as System>::AccountId,
+}
+
+#[derive(Debug, Decode, Eq, Event, PartialEq)]
+pub struct NewShareTypeEvent<T: SharesAtomic> {
+    pub org: T::OrgId,
+    pub share: T::ShareId,
+}
+
+#[derive(Debug, Decode, Eq, Event, PartialEq)]
+pub struct IssuanceEvent<T: SharesAtomic> {
+    pub org: T::OrgId,
+    pub share: T::ShareId,
+    pub account: <T as System>::AccountId,
+    pub amount: T::Share,
+}
+
+#[derive(Debug, Decode, Eq, Event, PartialEq)]
+pub struct BurnEvent<T: SharesAtomic> {
+    pub org: T::OrgId,
+    pub share: T::ShareId,
+    pub account: <T as System>::AccountId,
+    pub amount: T::Share,
+}
+
+#[derive(Debug, Decode, Eq, Event, PartialEq)]
+pub struct TotalSharesIssuedEvent<T: SharesAtomic> {
+    pub org: T::OrgId,
+    pub share: T::ShareId,
+    pub amount: T::Share,
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::Runtime;
     use sp_core::crypto::Pair;
     use sp_keyring::AccountKeyring;
 
     #[async_std::test]
+    #[ignore]
     async fn test_reserve_shares() {
         env_logger::init();
-        let signer = AccountKeyring::Eve.pair();
+        let signer = AccountKeyring::Alice.pair();
         let client = crate::build_client().await.unwrap();
         let xt = client.xt(signer.clone(), None).await.unwrap();
 
@@ -238,7 +183,7 @@ mod tests {
         let share = 1;
         let account = signer.public().into();
         let reserved = client
-            .profile(&org, &share, &account)
+            .profile(&(&org, &share), &account)
             .await
             .unwrap()
             .get_times_reserved();
@@ -246,10 +191,18 @@ mod tests {
         let extrinsic_success = xt
             .watch()
             .with_shares_atomic()
-            .submit(reserve_shares::<Runtime>(org, share, account.clone()))
+            .reserve_shares(org, share, account.clone())
             .await
             .unwrap();
-        let res = extrinsic_success.shares_reserved().unwrap().unwrap();
-        assert_eq!((org, share, account, reserved + 1), res);
+        let event = extrinsic_success.shares_reserved().unwrap().unwrap();
+        assert_eq!(
+            event,
+            SharesReservedEvent {
+                org,
+                share,
+                account,
+                reserved: reserved + 1
+            }
+        );
     }
 }
