b1640d0e518a890539611b7b60fc614b0fa934c5
David Craven <david@craven.ch>
2020-05-08 21:00:43 +0200

    Update atomic shares client.

--- client/examples/reserve_shares_and_watch.rs
+++ client/examples/reserve_shares_and_watch.rs
@@ -1,48 +1,35 @@
+use sp_core::crypto::Pair;
 use sp_keyring::AccountKeyring;
-use sunshine_client::shares_atomic::{reserve_shares, SharesAtomic};
-use sunshine_client::system::System;
-use sunshine_client::{ClientBuilder, Runtime};
-
-type AccountId = <Runtime as System>::AccountId;
-type OrgId = <Runtime as SharesAtomic>::OrgId;
-type ShareId = <Runtime as SharesAtomic>::ShareId;
+use sunshine_client::shares_atomic::*;
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
+    let signer = AccountKeyring::Eve.pair();
+    let client = sunshine_client::build_client().await?;
+    let xt = client.xt(signer.clone(), None).await?;
 
-    let alice_the_signer = AccountKeyring::Alice.pair();
+    let org = 1;
+    let share = 1;
+    let account = signer.public().into();
+    let reserved = client
+        .profile((org, share), &account)
+        .await?
+        .unwrap()
+        .get_times_reserved();
 
-    let reserves_alices_shares = AccountKeyring::Alice.to_account_id();
-
-    let organization: OrgId = 1u64;
-    let share_id: ShareId = 1u64;
-
-    let cli = ClientBuilder::new().build().await?;
-    let xt = cli.xt(alice_the_signer, None).await?;
     let extrinsic_success = xt
         .watch()
-        .events_decoder(|decoder| {
-            // for any primitive event with no type size registered
-            decoder.register_type_size::<u64>("OrgId")?;
-            decoder.register_type_size::<u64>("ShareId")?;
-            decoder.register_type_size::<(u64, u64, u64)>("IdentificationTuple")
-        })
-        .submit(reserve_shares::<Runtime>(
-            organization,
-            share_id,
-            reserves_alices_shares.clone().into(),
-        ))
+        .reserve_shares(org, share, &account)
         .await?;
-    let event = extrinsic_success
-        .find_event::<(OrgId, ShareId, AccountId, u32)>("SharesAtomic", "SharesReserved");
-    match event {
-        Some(Ok((org, share, account, amt))) => println!(
-            "Account {:?} reserved {:?} shares with share id {:?} for organization id {:?}",
-            account, amt, share, org
-        ),
-        Some(Err(err)) => println!("Failed to decode code hash: {}", err),
-        None => println!("Failed to find SharesAtomic::Reserve Event"),
-    }
+    let event = extrinsic_success.shares_reserved()?.unwrap();
+
+    assert_eq!(reserved + 1, event.reserved);
+
+    println!(
+        "Account {:?} reserved {:?} shares with share id {:?} for organization {:?}",
+        event.account, event.reserved, event.share, event.org,
+    );
+
     Ok(())
 }
