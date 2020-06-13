8baf9a5ed0014ebfb96fd3ff030accd20be369cb
4meta5 <asinghchrony@protonmail.com>
2020-06-13 15:06:27 -0400

    add back light client feature and remove subxt_test stuff because it is unusable, will add tests the same way as substrate identity example in future commits

--- client/src/srml/org.rs
+++ client/src/srml/org.rs
@@ -69,7 +69,7 @@ pub struct TotalIssuanceStore<T: Org> {
 pub struct ProfileStore<'a, T: Org> {
     #[store(returns = ShareProfile<T::Shares>)]
     pub org: T::OrgId,
-    pub account: &'a <T as System>::AccountId,
+    pub who: &'a <T as System>::AccountId,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, Store, Encode)]
@@ -92,70 +92,20 @@ pub struct RegisterFlatOrgCall<'a, T: Org> {
     pub members: &'a [<T as System>::AccountId],
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, Call, Encode)]
-pub struct RegisterWeightedOrgCall<'a, T: Org> {
-    pub sudo: Option<<T as System>::AccountId>,
-    pub parent_org: Option<T::OrgId>,
-    pub constitution: T::IpfsReference,
-    pub members: &'a [(<T as System>::AccountId, T::Shares)],
-}
-
-#[derive(Clone, Debug, Eq, PartialEq, Call, Encode)]
-pub struct IssueSharesCall<'a, T: Org> {
-    pub org: T::OrgId,
-    pub who: &'a <T as System>::AccountId,
-    pub amount: T::Shares,
-}
-
-#[derive(Clone, Debug, Eq, PartialEq, Call, Encode)]
-pub struct BurnSharesCall<'a, T: Org> {
-    pub org: T::OrgId,
-    pub who: &'a <T as System>::AccountId,
-    pub amount: T::Shares,
-}
-
-#[derive(Clone, Debug, Eq, PartialEq, Call, Encode)]
-pub struct BatchIssueSharesCall<'a, T: Org> {
-    pub org: T::OrgId,
-    pub new_accounts: &'a [(<T as System>::AccountId, T::Shares)],
-}
-
-#[derive(Clone, Debug, Eq, PartialEq, Call, Encode)]
-pub struct BatchBurnSharesCall<'a, T: Org> {
-    pub org: T::OrgId,
-    pub new_accounts: &'a [(<T as System>::AccountId, T::Shares)],
-}
-
-#[derive(Clone, Debug, Eq, PartialEq, Call, Encode)]
-pub struct LockSharesCall<'a, T: Org> {
-    pub org: T::OrgId,
-    pub who: &'a <T as System>::AccountId,
-}
-
-#[derive(Clone, Debug, Eq, PartialEq, Call, Encode)]
-pub struct UnlockSharesCall<'a, T: Org> {
-    pub org: T::OrgId,
-    pub who: &'a <T as System>::AccountId,
-}
-
-#[derive(Clone, Debug, Eq, PartialEq, Call, Encode)]
-pub struct ReserveSharesCall<'a, T: Org> {
-    pub org: T::OrgId,
-    pub who: &'a <T as System>::AccountId,
-}
-
-#[derive(Clone, Debug, Eq, PartialEq, Call, Encode)]
-pub struct UnreserveSharesCall<'a, T: Org> {
-    pub org: T::OrgId,
-    pub who: &'a <T as System>::AccountId,
-}
-
 #[derive(Clone, Debug, Eq, PartialEq, Event, Decode)]
 pub struct FlatOrgRegisteredEvent<T: Org> {
     pub caller: <T as System>::AccountId,
     pub new_id: T::OrgId,
     pub constitution: T::IpfsReference,
     pub total: u32,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Call, Encode)]
+pub struct RegisterWeightedOrgCall<'a, T: Org> {
+    pub sudo: Option<<T as System>::AccountId>,
+    pub parent_org: Option<T::OrgId>,
+    pub constitution: T::IpfsReference,
+    pub members: &'a [(<T as System>::AccountId, T::Shares)],
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, Event, Decode)]
@@ -166,18 +116,38 @@ pub struct WeightedOrgRegisteredEvent<T: Org> {
     pub total: T::Shares,
 }
 
+#[derive(Clone, Debug, Eq, PartialEq, Call, Encode)]
+pub struct IssueSharesCall<'a, T: Org> {
+    pub org: T::OrgId,
+    pub who: &'a <T as System>::AccountId,
+    pub amount: T::Shares,
+}
+
 #[derive(Clone, Debug, Eq, PartialEq, Event, Decode)]
 pub struct SharesIssuedEvent<T: Org> {
     pub org: T::OrgId,
-    pub account: <T as System>::AccountId,
+    pub who: <T as System>::AccountId,
+    pub amount: T::Shares,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Call, Encode)]
+pub struct BurnSharesCall<'a, T: Org> {
+    pub org: T::OrgId,
+    pub who: &'a <T as System>::AccountId,
     pub amount: T::Shares,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, Event, Decode)]
 pub struct SharesBurnedEvent<T: Org> {
     pub org: T::OrgId,
-    pub account: <T as System>::AccountId,
+    pub who: <T as System>::AccountId,
     pub amount: T::Shares,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Call, Encode)]
+pub struct BatchIssueSharesCall<'a, T: Org> {
+    pub org: T::OrgId,
+    pub new_accounts: &'a [(<T as System>::AccountId, T::Shares)],
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, Event, Decode)]
@@ -186,10 +156,46 @@ pub struct SharesBatchIssuedEvent<T: Org> {
     pub amount: T::Shares,
 }
 
+#[derive(Clone, Debug, Eq, PartialEq, Call, Encode)]
+pub struct BatchBurnSharesCall<'a, T: Org> {
+    pub org: T::OrgId,
+    pub old_accounts: &'a [(<T as System>::AccountId, T::Shares)],
+}
+
 #[derive(Clone, Debug, Eq, PartialEq, Event, Decode)]
 pub struct SharesBatchBurnedEvent<T: Org> {
     pub org: T::OrgId,
     pub amount: T::Shares,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Call, Encode)]
+pub struct LockSharesCall<'a, T: Org> {
+    pub org: T::OrgId,
+    pub who: &'a <T as System>::AccountId,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Event, Decode)]
+pub struct SharesLockedEvent<T: Org> {
+    pub org: T::OrgId,
+    pub who: <T as System>::AccountId,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Call, Encode)]
+pub struct UnlockSharesCall<'a, T: Org> {
+    pub org: T::OrgId,
+    pub who: &'a <T as System>::AccountId,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Event, Decode)]
+pub struct SharesUnlockedEvent<T: Org> {
+    pub org: T::OrgId,
+    pub who: <T as System>::AccountId,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Call, Encode)]
+pub struct ReserveSharesCall<'a, T: Org> {
+    pub org: T::OrgId,
+    pub who: &'a <T as System>::AccountId,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, Event, Decode)]
@@ -199,203 +205,15 @@ pub struct SharesReservedEvent<T: Org> {
     pub amount: T::Shares,
 }
 
+#[derive(Clone, Debug, Eq, PartialEq, Call, Encode)]
+pub struct UnreserveSharesCall<'a, T: Org> {
+    pub org: T::OrgId,
+    pub who: &'a <T as System>::AccountId,
+}
+
 #[derive(Clone, Debug, Eq, PartialEq, Event, Decode)]
 pub struct SharesUnReservedEvent<T: Org> {
     pub org: T::OrgId,
     pub who: <T as System>::AccountId,
     pub amount: T::Shares,
-}
-
-#[derive(Clone, Debug, Eq, PartialEq, Event, Decode)]
-pub struct SharesLockedEvent<T: Org> {
-    pub org: T::OrgId,
-    pub who: <T as System>::AccountId,
-}
-
-#[derive(Clone, Debug, Eq, PartialEq, Event, Decode)]
-pub struct SharesUnlockedEvent<T: Org> {
-    pub org: T::OrgId,
-    pub who: <T as System>::AccountId,
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::runtime::{Runtime, RuntimeExtra};
-
-    subxt_test!({
-        name: issue_shares,
-        runtime: Runtime,
-        extra: RuntimeExtra,
-        /*state: {
-            issuance: TotalIssuanceStore {
-                org: 1,
-                share: 1,
-            },
-        },*/
-        step: {
-            call: IssueSharesCall {
-                org: 1,
-                share: 1,
-                who: &alice,
-                amount: 10,
-            },
-            event: SharesIssuedEvent {
-                org: 1,
-                share: 1,
-                account: alice.clone(),
-                amount: 10,
-            },
-            /*assert: {
-                assert_eq!(pre.issuance + 20, post.issuance);
-            },*/
-        },
-        step: {
-            call: BurnSharesCall {
-                org: 1,
-                share: 1,
-                who: &alice,
-                amount: 10,
-            },
-            event: SharesBurnedEvent {
-                org: 1,
-                share: 1,
-                account: alice.clone(),
-                amount: 10,
-            },
-            /*assert: {
-                assert_eq!(pre.issuance - 20, post.issuance);
-            },*/
-        }
-    });
-
-    subxt_test!({
-        name: batch_issue_shares,
-        runtime: Runtime,
-        extra: RuntimeExtra,
-        /*state: {
-            issuance: TotalIssuanceStore {
-                org: 1,
-                share: 1,
-            },
-        },*/
-        step: {
-            call: BatchIssueSharesCall {
-                org: 1,
-                share: 1,
-                new_accounts: &[(alice.clone(), 10), (bob.clone(), 10)],
-            },
-            event: SharesBatchIssuedEvent {
-                org: 1,
-                share: 1,
-                amount: 20,
-            },
-            /*assert: {
-                assert_eq!(pre.issuance + 20, post.issuance);
-            },*/
-        },
-        step: {
-            call: BatchBurnSharesCall {
-                org: 1,
-                share: 1,
-                new_accounts: &[(alice.clone(), 10), (bob.clone(), 10)],
-            },
-            event: SharesBatchBurnedEvent {
-                org: 1,
-                share: 1,
-                amount: 20,
-            },
-            /*assert: {
-                assert_eq!(pre.issuance - 20, post.issuance);
-            },*/
-        }
-    });
-
-    subxt_test!({
-        name: reserve_shares,
-        runtime: Runtime,
-        extra: RuntimeExtra,
-        /*state: {
-            profile: ProfileStore {
-                prefix: UUID2::new(1, 1),
-                account_id: &alice,
-            },
-        },*/
-        step: {
-            call: ReserveSharesCall {
-                org: 1,
-                share: 1,
-                who: &alice,
-            },
-            event: SharesReservedEvent {
-                org: 1,
-                share: 1,
-                account: alice.clone(),
-                reserved: event.reserved, //pre.profile.times_reserved() + 1,
-            },
-            /*assert: {
-                assert_eq!(pre.profile.times_reserved() + 1, post.profile.times_reserved());
-            },*/
-        },
-        step: {
-            call: UnreserveSharesCall {
-                org: 1,
-                share: 1,
-                who: &alice,
-            },
-            event: SharesUnReservedEvent {
-                org: 1,
-                share: 1,
-                account: alice.clone(),
-                reserved: event.reserved, //pre.profile.times_reserved() - 1,
-            },
-            /*assert: {
-                assert_eq!(pre.profile.times_reserved() - 1, post.profile.times_reserved());
-            },*/
-        }
-    });
-
-    subxt_test!({
-        name: lock_shares,
-        runtime: Runtime,
-        extra: RuntimeExtra,
-        /*state: {
-            profile: ProfileStore {
-                prefix: UUID2::new(1, 1),
-                account_id: &alice,
-            },
-        },*/
-        step: {
-            call: LockSharesCall {
-                org: 1,
-                share: 1,
-                who: &alice,
-            },
-            event: SharesLockedEvent {
-                org: 1,
-                share: 1,
-                account: alice.clone(),
-            },
-            /*assert: {
-                assert_eq!(pre.profile.is_unlocked(), true);
-                assert_eq!(post.profile.is_unlocked(), false);
-            },*/
-        },
-        step: {
-            call: UnlockSharesCall {
-                org: 1,
-                share: 1,
-                who: &alice,
-            },
-            event: SharesUnlockedEvent {
-                org: 1,
-                share: 1,
-                account: alice.clone(),
-            },
-            /*assert: {
-                assert_eq!(pre.profile.is_unlocked(), false);
-                assert_eq!(post.profile.is_unlocked(), true);
-            },*/
-        }
-    });
 }
