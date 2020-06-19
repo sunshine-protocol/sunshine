6f011a49e6d76a8df5b404d83a192656563c1b41
4meta5 <asinghchrony@protonmail.com>
2020-06-18 21:35:04 -0400

    client still returns EventNotFound even after recent refactor as per #95

--- client/examples/org.rs
+++ client/examples/org.rs
@@ -5,16 +5,20 @@ use ipfs_embed::{Config, Store};
 use ipld_block_builder::{BlockBuilder, Codec};
 use keystore::{DeviceKey, KeyStore, Password};
 use sp_core::sr25519::Pair;
-use substrate_subxt::ClientBuilder;
-use sunshine_client::{Error, Runtime, SunClient};
+use substrate_subxt::{sp_runtime, ClientBuilder};
+use sunshine_client::{Error, Extra, Runtime, SunClient};
 // use libipld::cid::{Cid, Codec};
 // use libipld::multihash::Sha2_256;
 // use utils_identity::cid::CidBytes;
 
+pub type RuntimeExtra = Extra<Runtime>;
+
 #[async_std::main]
 async fn main() -> Result<(), Error> {
     env_logger::init();
-    //let subxt = ClientBuilder::<Runtime>::new().build().await?;
+    let subxt = ClientBuilder::<Runtime, sp_runtime::MultiSignature, RuntimeExtra>::new()
+        .build()
+        .await?;
     let db = sled::open("/tmp/db")?;
     let ipld_tree = db.open_tree("ipld_tree")?;
     let config = Config::from_tree(ipld_tree);
@@ -27,15 +31,15 @@ async fn main() -> Result<(), Error> {
         &DeviceKey::from_seed(alice_seed),
         &Password::from("password".to_string()),
     )?;
-    //let client = SunClient::<_, _, _, Pair, _>::new(keystore, subxt, ipld);
+    let client = SunClient::<_, _, _, Pair, _>::new(keystore, subxt, ipld);
     let account_id = sp_keyring::AccountKeyring::Alice.to_account_id();
-    println!("{}", account_id);
-    //client.issue_shares(1u64, account_id, 10u64).await?;
+    println!("This is Alice's Account Identifier: {}", account_id);
+    let event = client.issue_shares(1u64, account_id, 10u64).await?;
 
-    // println!(
-    //     "Account {:?} was issued {:?} shares for organization {:?}",
-    //     event.who, event.shares, event.organization,
-    // );
+    println!(
+        "Account {:?} was issued {:?} shares for organization {:?}",
+        event.who, event.shares, event.organization,
+    );
 
     Ok(())
 }
