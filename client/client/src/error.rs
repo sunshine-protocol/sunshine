1bdc9043490e681adefa251a10320883b5f5a6f8
4meta5 <asinghchrony@protonmail.com>
2020-07-16 15:16:03 -0400

    remove superfluous error type

--- client/client/src/error.rs
+++ client/client/src/error.rs
@@ -4,8 +4,6 @@ use thiserror::Error;
 pub enum Error {
     #[error("event not found")]
     EventNotFound,
-    #[error("Custom description cannot be parsed from string")]
-    ParseCodecError,
     #[error("Number cannot be parsed from string")]
     ParseIntError,
 }
